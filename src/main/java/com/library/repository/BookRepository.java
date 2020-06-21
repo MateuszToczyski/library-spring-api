1606fd45b9f32130ef018cd48cfda7f25b53e7cc
Mateusz Toczyski <toczyski.mateusz@gmail.com>
2020-06-21 15:36:50 +0200

    Remove redundant code

--- src/main/java/com/library/repository/BookRepository.java
+++ src/main/java/com/library/repository/BookRepository.java
@@ -4,7 +4,4 @@ import com.library.domain.Book;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
-
-    //@Override
-
 }
