2491d43842bd8ae321f1145972b4b61bf482101b
Mateusz Toczyski <toczyski.mateusz@gmail.com>
2020-06-22 08:54:01 +0200

    Modify BookController endpoints

--- src/main/java/com/library/controller/BookController.java
+++ src/main/java/com/library/controller/BookController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
-@RequestMapping("/v1/book")
+@RequestMapping("/v1/books")
 @CrossOrigin(origins = "*")
 @AllArgsConstructor
 public class BookController {
@@ -18,12 +18,12 @@ public class BookController {
     private final DbService dbService;
     private final BookMapper bookMapper;
 
-    @RequestMapping(method = RequestMethod.GET, value = "getBook")
-    public BookDto getBook(@RequestParam Long id) {
+    @RequestMapping(method = RequestMethod.GET, value = "{id}")
+    public BookDto getBook(@PathVariable Long id) {
         return bookMapper.mapToBookDto(dbService.getBook(id).orElseThrow(BookNotFoundException::new));
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "createBook", consumes = APPLICATION_JSON_VALUE)
+    @RequestMapping(method = RequestMethod.POST, consumes = APPLICATION_JSON_VALUE)
     public BookDto createBook(@RequestBody BookDto bookDto) {
         return bookMapper.mapToBookDto(dbService.saveBook(bookMapper.mapToBook(bookDto)));
     }
